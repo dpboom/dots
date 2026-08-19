6ed336be24cba7e40a2b8656b8fe586fbb16dc35
dpboom <bradh3mmk@gmail.com>
2026-08-19 05:10:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 mv ~/dots/hyprland.lua ~/.config/hypr/hyprland.lua
-mv ~/dots/kitty.conf ~/.config/kitty/kitty.conf
-rm README.md
+mkdir -p .config/ghostty/themes
+mv ~/dots/kdefault ~/.config/ghostty/themes/kdefault
+mv ~/dots/config.ghostty ~/.config/ghostty/config.ghostty
 rm -rf ~/dots
 sudo reboot
