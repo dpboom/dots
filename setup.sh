2789080b94d2f5be0e681d66a106f35aa4bf4856
dpboom <bradh3mmk@gmail.com>
2026-08-20 00:54:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 mv ~/dots/hyprland.lua ~/.config/hypr/hyprland.lua
 mv ~/dots/kitty.conf ~/.config/kitty/kitty.conf
+xdg-mime default mpv.desktop image/*
+xdg-mime default mpv.desktop video/*
 rm -rf ~/dots
 sudo reboot
