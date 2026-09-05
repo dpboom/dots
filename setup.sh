69c6fdf4a16c358aad4a7ae6b5b013b709954b02
dpboom <bradh3mmk@gmail.com>
2026-09-05 16:19:52 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.config/waybar
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
-mkdir -p ~/.config/fastfetch/config.jsonc
+mkdir -p ~/.config/fastfetch
 mv hyprland.lua ~/.config/hypr/hyprland.lua
 mv kitty.conf ~/.config/kitty/kitty.conf
 mv config.jsonc ~/.config/waybar/config.jsonc
 mv style.css ~/.config/waybar/style.css
+mv 2config.jsonc. ~/.config/fastfetch
+mv ~/.config/fastfetch/2config.jsonc ~/.config/fastfetch/config.jsonc
 xdg-mime default mpv.desktop image/*
 xdg-mime default mpv.desktop video/*
 rm -rf ../dots
