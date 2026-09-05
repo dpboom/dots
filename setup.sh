2f4cdcf40ec53e1cd84a6f225fcf2029c4f22214
dpboom <bradh3mmk@gmail.com>
2026-09-05 16:14:51 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.config/waybar
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
+mkdir -p ~/.config/fastfetch/config.jsonc
 mv hyprland.lua ~/.config/hypr/hyprland.lua
 mv kitty.conf ~/.config/kitty/kitty.conf
 mv config.jsonc ~/.config/waybar/config.jsonc
