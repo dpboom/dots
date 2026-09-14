4282e2db223507848aee44bb22bc11d2dca30f94
dpboom <bradh3mmk@gmail.com>
2026-09-14 14:58:56 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mv hyprland.lua ~/.config/hypr/hyprland.lua
 mv kitty.conf ~/.config/kitty/kitty.conf
 mv config.jsonc ~/.config/waybar/config.jsonc
 mv style.css ~/.config/waybar/style.css
-mv 2config.jsonc. ~/.config/fastfetch
+mv config2.jsonc. ~/.config/fastfetch
 mv ~/.config/fastfetch/2config.jsonc ~/.config/fastfetch/config.jsonc
 xdg-mime default mpv.desktop image/*
 xdg-mime default mpv.desktop video/*
