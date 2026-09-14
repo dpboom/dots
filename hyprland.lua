9b22d12fada17957e2367d774606ac33d5eff54f
dpboom <bradh3mmk@gmail.com>
2026-09-14 15:41:46 +0300

    Updated README.md

--- hyprland.lua
+++ hyprland.lua
@@ -69,7 +69,7 @@ hl.config({ -- gaps between windows/screen borders
   },
 
   decoration = { -- round corners
-    rounding         = 9, --  0 for sharp corners
+    rounding         = 7, --  0 for sharp corners
     rounding_power   = 3,
 
     -- dont change that
