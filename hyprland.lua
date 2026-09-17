d3beaacf1813bd5a1d23b1572b5c700688f5cfd1
dpboom <bradh3mmk@gmail.com>
2026-09-17 18:32:52 +0300

    Updated README.md

--- hyprland.lua
+++ hyprland.lua
@@ -17,7 +17,8 @@ hl.monitor({ -- fetch monitor name via the `hyprctl monitors` command
 -- Set programs that you use
 local terminal    = "kitty" -- terminal
 local fileManager = "yazi" -- file manager
-local menu        = "rofi -show drun" -- app menu
+local menu        = "fuzzel" -- app menu
+local browser     = "zen-browser" -- browser
 
 
 -------------------
@@ -200,13 +201,13 @@ hl.device({
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(browser))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", -- you can change keybinds by replacing the letter in the keybind with a preferred bind
-  hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("kitty -e yazi")) -- replace 'kitty -e yazi' with your preferred file manager
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("kitty -e yazi")) -- replace 'kitty' with your preferred file manager
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("rofi -show drun")) -- replace rofi with your app menu
 hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd("zen-browser")) -- replace zen browser with your browser [the command that opens it]
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
