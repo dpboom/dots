72da3105b4c4ec8b310251ff331f55f58d90b81c
dpboom <bradh3mmk@gmail.com>
2026-09-19 17:29:38 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ mkdir -p ~/.config/waybar
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/fastfetch
-mv hyprland.lua ~/.config/hypr/
-mv kitty.conf ~/.config/kitty/
-mv config.jsonc ~/.config/waybar/
+mv hyprland.lua ~/.config/hypr
+mv kitty.conf ~/.config/kitty
+mv config.jsonc ~/.config/waybar
 mv style.css ~/.config/waybar/
-mv config2.jsonc. ~/.config/fastfetch/
+mv config2.jsonc. ~/.config/fastfetch
 mv ~/.config/fastfetch/config2.jsonc ~/.config/fastfetch/config.jsonc
 pkill hyprland
