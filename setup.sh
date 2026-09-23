231b8a3c1bba1d3e3e9e3bdac46a6c69770e4ce7
dpboom <bradh3mmk@gmail.com>
2026-09-23 22:11:53 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ mv hyprland.lua ~/.config/hypr
 mv kitty.conf ~/.config/kitty
 mv config.jsonc ~/.config/waybar
 mv style.css ~/.config/waybar/
-mv config2.jsonc. ~/.config/fastfetch
+mv config2.jsonc ~/.config/fastfetch
 mv ~/.config/fastfetch/config2.jsonc ~/.config/fastfetch/config.jsonc
 pkill hyprland
